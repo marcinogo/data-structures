6c874e9c7cbd23b32b8dffa304fa9009823919b4
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 16:37:16 +0100

    Declare declare size, dynamicArray fields and static filed FACTOR

--- src/main/java/com/codecool/krk/dynamicarray/DynamicArray.java
+++ src/main/java/com/codecool/krk/dynamicarray/DynamicArray.java
@@ -1,4 +1,9 @@
 package com.codecool.krk.dynamicarray;
 
 public class DynamicArray<T> {
+    // specify how many time increase size of dynamicArray
+    private static int FACTOR;
+
+    private int size;
+    private T[] dynamicArray;
 }
