023c05b587464566791a2fa34d45f70b04199b27
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 16:42:13 +0100

    Correct modifier of filed FACTOR from static to final

--- src/main/java/com/codecool/krk/dynamicarray/DynamicArray.java
+++ src/main/java/com/codecool/krk/dynamicarray/DynamicArray.java
@@ -2,7 +2,7 @@ package com.codecool.krk.dynamicarray;
 
 public class DynamicArray<T> {
     // specify how many time increase size of dynamicArray
-    private static int FACTOR;
+    private final int FACTOR;
 
     private int size;
     private T[] dynamicArray;
