24381fdd061a4bb17aad7ab96badc3d9d6b921eb
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 22:22:22 +0100

    Implement setters and getters for all fields

--- src/main/java/com/codecool/krk/stack/Stack.java
+++ src/main/java/com/codecool/krk/stack/Stack.java
@@ -7,8 +7,32 @@ public class Stack<T> {
 
     public Stack(int size) {
         this.size = size;
-        
+
         this.top = -1;
         this.stack = (T[]) new Object[this.size];
+    }
+
+    public T[] getStack() {
+        return stack;
+    }
+
+    public void setStack(T[] stack) {
+        this.stack = stack;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
     }
 }
