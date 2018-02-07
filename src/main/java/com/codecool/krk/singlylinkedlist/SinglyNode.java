84f6639c1c3c248d02e40deb12639b6cf61248fd
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 17:52:45 +0100

    Declare and implement parametrized constructor

--- src/main/java/com/codecool/krk/singlylinkedlist/SinglyNode.java
+++ src/main/java/com/codecool/krk/singlylinkedlist/SinglyNode.java
@@ -3,4 +3,8 @@ package com.codecool.krk.singlylinkedlist;
 public class SinglyNode<T> {
     private T data;
     private SinglyNode<T> nextNode;
+
+    public SinglyNode(T data) {
+        this.data = data;
+    }
 }
