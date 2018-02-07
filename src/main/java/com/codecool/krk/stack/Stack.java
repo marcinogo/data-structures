d09ea588ce7d09bf302952ea8be863ef5852b69b
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 22:21:50 +0100

    Implement parametrized constructor

--- src/main/java/com/codecool/krk/stack/Stack.java
+++ src/main/java/com/codecool/krk/stack/Stack.java
@@ -4,4 +4,11 @@ public class Stack<T> {
     private T[] stack;
     private int size;
     private int top;
+
+    public Stack(int size) {
+        this.size = size;
+        
+        this.top = -1;
+        this.stack = (T[]) new Object[this.size];
+    }
 }
