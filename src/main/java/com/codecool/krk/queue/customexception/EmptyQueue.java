bc6636656802087a7fa8c24631533eb53a9148fb
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 22:38:57 +0100

    Implement all exception constructors

--- src/main/java/com/codecool/krk/queue/customexception/EmptyQueue.java
+++ src/main/java/com/codecool/krk/queue/customexception/EmptyQueue.java
@@ -1,4 +1,22 @@
 package com.codecool.krk.queue.customexception;
 
 public class EmptyQueue extends Exception {
+    public EmptyQueue() {
+    }
+
+    public EmptyQueue(String message) {
+        super(message);
+    }
+
+    public EmptyQueue(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmptyQueue(Throwable cause) {
+        super(cause);
+    }
+
+    public EmptyQueue(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
