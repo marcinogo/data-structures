f628e4796e468c6afec732b8029b09bce7ad1b05
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 22:17:48 +0100

    Implement all exception constructors

--- src/main/java/com/codecool/krk/stack/customexception/StackUnderflow.java
+++ src/main/java/com/codecool/krk/stack/customexception/StackUnderflow.java
@@ -1,4 +1,22 @@
 package com.codecool.krk.stack.customexception;
 
 public class StackUnderflow extends Exception{
+    public StackUnderflow() {
+    }
+
+    public StackUnderflow(String message) {
+        super(message);
+    }
+
+    public StackUnderflow(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StackUnderflow(Throwable cause) {
+        super(cause);
+    }
+
+    public StackUnderflow(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
