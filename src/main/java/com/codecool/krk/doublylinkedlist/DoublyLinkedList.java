14b4dbac028f8d11929ac5f299760b509a87b959
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-07 21:41:05 +0100

    Implement setters and getters for all fields

--- src/main/java/com/codecool/krk/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/com/codecool/krk/doublylinkedlist/DoublyLinkedList.java
@@ -8,4 +8,28 @@ public class DoublyLinkedList<T> {
     public DoublyLinkedList() {
         this.length = 0;
     }
+
+    public DoublyNode<T> getHead() {
+        return head;
+    }
+
+    public void setHead(DoublyNode<T> head) {
+        this.head = head;
+    }
+
+    public DoublyNode<T> getLast() {
+        return last;
+    }
+
+    public void setLast(DoublyNode<T> last) {
+        this.last = last;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
