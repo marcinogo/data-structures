de5147a3712780ab9d4c42277705e18da9ca39c0
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-08 21:46:06 +0100

    Remove constructor with parameter of type Node

--- src/main/java/com/codecool/krk/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/codecool/krk/binarysearchtree/BinarySearchTree.java
@@ -3,10 +3,6 @@ package com.codecool.krk.binarysearchtree;
 public class BinarySearchTree {
     private Node root;
 
-    public BinarySearchTree(Node root) {
-        this.root = root;
-    }
-
     public BinarySearchTree(Integer value) {
         this.root = new Node(value);
     }
