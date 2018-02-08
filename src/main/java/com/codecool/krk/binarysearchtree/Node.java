8d59cb02ebbf4fa56038f517b1356e4de1f9fdda
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-08 21:40:48 +0100

    Implement setters and getters for all fields

--- src/main/java/com/codecool/krk/binarysearchtree/Node.java
+++ src/main/java/com/codecool/krk/binarysearchtree/Node.java
@@ -8,4 +8,28 @@ public class Node {
     public Node(Integer value) {
         this.value = value;
     }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
 }
