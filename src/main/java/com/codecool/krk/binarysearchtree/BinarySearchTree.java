67a23404cea07467562815e8809c8d99f2c4934f
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-08 21:44:17 +0100

    Implement two parametrized constructors

--- src/main/java/com/codecool/krk/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/codecool/krk/binarysearchtree/BinarySearchTree.java
@@ -2,4 +2,12 @@ package com.codecool.krk.binarysearchtree;
 
 public class BinarySearchTree {
     private Node root;
+
+    public BinarySearchTree(Node root) {
+        this.root = root;
+    }
+
+    public BinarySearchTree(Integer value) {
+        this.root = new Node(value);
+    }
 }
